fda4f5f1bc299d4f428bb4a55ca044aef5930ab1
Isaac Colls <colls_isaac@hotmail.com>
2025-07-01 22:03:35 -0400

    feat: Add README commands section and initial build script; implement main functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SetaSense
+
+## commands
+
+- build
---
+++ main.c
@@ -0,0 +1,22 @@
+#include <16F877A.h>
+#fuses HS, NOWDT, NOPROTECT, NOCPD
+#use delay(clock = 20000000)
+#use fast_io(b)
+
+char customBlink(int delay) : void
+{
+  output_high(pin_b7);
+  delay_ms(delay);
+  output_low(pin_b7);
+  delay_ms(delay);
+}
+
+void main()
+{
+  port_b_pullups(true);
+  set_tris_b(0);
+  while (true)
+  {
+    customBlink(150, 110);
+  }
+}
