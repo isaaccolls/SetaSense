52a397c3552633348457fff3f236f47dbab9addb
Isaac Colls <colls_isaac@hotmail.com>
2025-07-23 22:50:11 -0400

    feat: Add LCD initialization and display messages in main function

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <16F877A.h>
 #fuses HS, NOWDT, NOPROTECT, NOCPD
 #use delay(clock = 20Mhz, crystal)
+#include <lcd.c>
 #use fast_io(b)
+
+#define LCD_ENABLE_PIN PIN_D3
+#define LCD_RS_PIN PIN_D1
+#define LCD_RW_PIN PIN_D2
+#define LCD_DATA4 PIN_D4
+#define LCD_DATA5 PIN_D5
+#define LCD_DATA6 PIN_D6
+#define LCD_DATA7 PIN_D7
 
 char customBlink(int delay)
 {
@@ -15,6 +24,10 @@ void main()
 {
   port_b_pullups(true);
   set_tris_b(0);
+  lcd_init();
+  lcd_putc("\fHola Mundo");
+  lcd_gotoxy(1, 2);
+  lcd_putc("Desde CCS :)");
   while (true)
   {
     customBlink(1000);
